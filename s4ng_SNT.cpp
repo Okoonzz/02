6afa41eeafff447813078445b828d056afe6e1c8
Thành Công <90641599+Okoonzz@users.noreply.github.com>
2022-12-21 10:24:04 +0700

    Update s4ng_SNT.cpp

--- s4ng_SNT.cpp
+++ s4ng_SNT.cpp
@@ -1,30 +1,3 @@
-// #include<iostream>
-
-// using namespace std;
-
-// int x[11];
-// void printArray(int a[], int n){
-// 	for (int i = 1; i <= n; i++){
-// 		cout << a[i];
-// 	}
-// 	cout <<" ";
-// }
-// void dequy(int h, int k, int n){
-// 	for (int i = x[h-1] + 1; i <= n - (k-h); i++){
-// 		x[h] = i;
-// 		if (h == k) printArray(x, k);
-// 		else {
-// 			dequy(h+1, k, n);
-// 		}
-// 	}
-// }
-// int main(){
-// 	int n, k;
-// 	cin >> n >> k;
-// 	x[0] = 0;
-// 	dequy(1, k, n);
-// }
-
 /* Kiểm tra trong một đoạn có bao nhiêu SNT 
 mà nghịch đảo của nó cũng SNT*/
 #include <iostream>
